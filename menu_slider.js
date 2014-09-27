0e6ae4165be0be0fad102b64e3c52c97261f7583
marcusmolchany <marcusmolchany@gmail.com>
2014-09-27 01:57:33 -0400

    initial commit for menu-slider

---
+++ menu_slider.js
@@ -0,0 +1,12 @@
+function slideMenu() {
+	var mainContent = document.getElementById('main-content');
+	var menuWrapper = document.getElementById('menu-wrapper');
+
+	//http://stackoverflow.com/questions/2739667/add-another-class-to-a-div-with-javascript
+  //http://stackoverflow.com/questions/5898656/test-if-an-element-contains-a-class
+	menuWrapper.className = menuWrapper.classList.contains('open') ? 'closed':'open';
+	mainContent.className = mainContent.classList.contains('open') ? 'closed':'open';
+}
+
+var menuButton = document.getElementById('menu-slider-button');
+menuButton.addEventListener('click', slideMenu, false);
