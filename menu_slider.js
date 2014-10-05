10e839f58eed08429672ff3a8f1486bd35238c82
Jean de Klerk <jadekler@gmail.com>
2014-10-05 10:24:58 -0400

    Code cleanup

--- menu_slider.js
+++ menu_slider.js
@@ -4,36 +4,34 @@ function slideMenu() {
 
   //http://stackoverflow.com/questions/2739667/add-another-class-to-a-div-with-javascript
   //http://stackoverflow.com/questions/5898656/test-if-an-element-contains-a-class
-  menuWrapper.className = menuWrapper.classList.contains('open') ? 'closed':'open';
-  mainContent.className = mainContent.classList.contains('open') ? 'closed':'open';
+  menuWrapper.className = menuWrapper.classList.contains('open') ? 'closed' : 'open';
+  mainContent.className = mainContent.classList.contains('open') ? 'closed' : 'open';
 }
 
 function animateHamburger() {
   var hamburgerButton = this;
-  hamburgerButton.className = hamburgerButton.classList.contains('open') ? 'closed':'open';
+  hamburgerButton.className = hamburgerButton.classList.contains('open') ? 'closed' : 'open';
 }
 
 function selectMenuItem() {
   var selectedMenuItem = this;
+  var menuItems = document.getElementById('menu').children;
 
-  selectedMenuItem.className = 'selected';
+  [].forEach.call(menuItems, function(value, index) {
+    menuItems[index].className = menuItems[index] == selectedMenuItem ? 'selected' : '';
+  });
+}
 
-  var menu = document.getElementById('menu');
-  var menuItems = menu.children;
+function setupEventListeners() {
+  var hamburgerButton = document.getElementById('menu-slider-hamburger');
+  hamburgerButton.addEventListener('click', slideMenu, false);
+  hamburgerButton.addEventListener('click', animateHamburger, false);
+
+  var menuItems = document.getElementById('menu').children;
+
   for (var i = 0; i < menuItems.length; i++) {
-    if (menuItems[i] != this) {
-      menuItems[i].className = '';
-    }
+    menuItems[i].addEventListener('click', selectMenuItem, false);
   }
 }
 
-var hamburgerButton = document.getElementById('menu-slider-hamburger');
-hamburgerButton.addEventListener('click', slideMenu, false);
-hamburgerButton.addEventListener('click', animateHamburger, false);
-
-var menu = document.getElementById('menu');
-var menuItems = menu.children;
-
-for (var i = 0; i < menuItems.length; i++) {
-  menuItems[i].addEventListener('click', selectMenuItem, false);
-}
+setupEventListeners();
